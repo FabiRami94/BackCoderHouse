696fcd6de230bb27276a2fa2ebe490f5221b9063
FabiRami94 <fabioerv_1994@hotmail.com>
2024-03-23 21:52:51 -0500

    inicio interior de cards

---
+++ Client/src/components/card/Card.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+
+const Card = () => {
+    return(
+        <h1>Hola soy una Card</h1>
+    )
+};
+
+export default Card;
---
+++ Client/src/components/cards/Cards.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+import Card from "../card/Card.jsx";
+
+const Cards = () => {
+    return(
+        <Card></Card>
+    )
+};
+
+export default Cards;
